4eb0aa8f1091cb4003fb73aa696d6948a7ad7d6d
cesar sousa junior <cesarsonline@gmail.com>
2013-08-25 12:23:24 -0300

    ...

---
+++ src/br/com/ammf/model/Categoria.java
@@ -0,0 +1,47 @@
+package br.com.ammf.model;
+
+import java.util.UUID;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "categoria")
+public class Categoria {
+	
+	@Id
+	@GeneratedValue
+	private long id;
+	
+	@Column(length = 45)
+	private String uuid = UUID.randomUUID().toString();
+	
+	private String descricao;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getUuid() {
+		return uuid;
+	}
+
+	public void setUuid(String uuid) {
+		this.uuid = uuid;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+}
